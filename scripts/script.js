1e33466d1f01dc63db23e8afdae0b1b63c449b93
Keanan_Oliver <151459049+keanan557@users.noreply.github.com>
2024-11-12 14:33:44 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// script for nav toggle
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
